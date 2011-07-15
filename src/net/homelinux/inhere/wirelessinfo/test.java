2104648eb4b42f97bb423069ed6dfd552886399c
pieter.linde <pieter.linde@1e14eff0-41d3-32bc-a52b-4516c52b40c5>
2011-07-15 06:35:59 +0000

    Added More event to 2nd activity. Possibly broken app.

--- src/net/homelinux/inhere/wirelessinfo/test.java
+++ src/net/homelinux/inhere/wirelessinfo/test.java
@@ -4,36 +4,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
 
 public class test extends Activity {
 	
 	String PUBLIC_STATIC_STRING_IDENTIFIER = null;
+	private EditText InputFileName;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test);
 		
+		InputFileName = (EditText) findViewById(R.id.inputFileName);
+	}
+	
+	public void onClickTest(View v) {
+		trace("test: onClickStartTest: Start.");
+		
 		Bundle b = this.getIntent().getExtras();
         String s = b.getString("IDENT");
-        trace("From parent intent = "+s);
-		
-		
-		/*Intent resultIntent = new Intent();
-		String tabIndexValue = "inside";
-		resultIntent.putExtra(PUBLIC_STATIC_STRING_IDENTIFIER, tabIndexValue);
-		setResult(Activity.RESULT_OK, resultIntent);
-		finish();*/
+        String s1 = b.getString("IDENT1");
+        trace("From parent intent = "+s+", second string = "+s1);
+        
+        String FileName = InputFileName.getText().toString();
 		
 		try{
 			Intent resultIntent = new Intent(getApplicationContext(),test.class);
 			resultIntent.putExtra(PUBLIC_STATIC_STRING_IDENTIFIER, "data_to_activity");
+			resultIntent.putExtra("FILE_NAME", FileName);
 			setResult(Activity.RESULT_OK, resultIntent);				
 			finish();
 		}catch (Exception e){
 			trace("test intent eror ."+e.getMessage());
-		}	
+		}
 		trace("test intent close.");
 	}
 	
