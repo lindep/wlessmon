2ec03856d0734637d69ccb98a91ceba96ea8a631
pieter.linde <pieter.linde@1e14eff0-41d3-32bc-a52b-4516c52b40c5>
2011-07-17 09:50:49 +0000

    Added second activity tests.

--- trunk/src/net/homelinux/inhere/wirelessinfo/test.java
+++ trunk/src/net/homelinux/inhere/wirelessinfo/test.java
@@ -8,13 +8,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class test extends Activity {
 	
 	String PUBLIC_STATIC_STRING_IDENTIFIER = null;
 	private EditText InputFileName;
+	private Button connectFtpButton;
+	private ToggleButton ftpActionButton;
+	LoginDetails serverLogin = null;
+	private ThrPutTest mCurrentThrPutTask = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,8 @@ public class test extends Activity {
 		
 		InputFileName = (EditText) findViewById(R.id.inputFileName);
 		
+		connectFtpButton = (Button) findViewById(R.id.connectFtp);
+		
 		Spinner spinner = (Spinner) findViewById(R.id.fileSizeSpinner);
 	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
 	            this, R.array.fileSize_array, android.R.layout.simple_spinner_item);
@@ -30,9 +39,22 @@ public class test extends Activity {
 	    spinner.setAdapter(adapter);
 	    
 	    spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+	    
+	    Bundle b = this.getIntent().getExtras();
+		if (b.getBoolean("hostStatus")) {
+			serverLogin = new LoginDetails();
+	        serverLogin.setHost(b.getString("host"));
+	        serverLogin.setPort(b.getInt("port"));
+	        serverLogin.setId(b.getString("id"));
+	        serverLogin.setPasswd(b.getString("passwd"));
+        
+	        trace("WirelessInfo: onClickThrputTest: End. Got server to test = "+serverLogin.getHost());
+		} else {
+			Toast.makeText(this, "Please set Login Details first!", Toast.LENGTH_SHORT).show();
+		}
 	}
 	
-	public void onClickTest(View v) {
+	public void onClickBack(View v) {
 		trace("test: onClickStartTest: Start.");
 		
 		Bundle b = this.getIntent().getExtras();
@@ -55,15 +77,65 @@ public class test extends Activity {
 	}
 	
 	public class MyOnItemSelectedListener implements OnItemSelectedListener {
-
 	    public void onItemSelected(AdapterView<?> parent,
 	        View view, int pos, long id) {
-	      trace("The planet is " +parent.getItemAtPosition(pos).toString());
+	      trace("The file size = " +parent.getItemAtPosition(pos).toString());
 	    }
-
 	    public void onNothingSelected(AdapterView parent) {
 	      // Do nothing.
 	    }
+	}
+	
+	public void onClickThrputTest(View v) {
+		trace("WirelessInfo: onClickThrputTest: Start.");
+		
+		Bundle b = this.getIntent().getExtras();
+		if (b.getBoolean("hostStatus")) {
+			LoginDetails serverLogin = new LoginDetails();
+	        serverLogin.setHost(b.getString("host"));
+	        serverLogin.setPort(b.getInt("port"));
+	        serverLogin.setId(b.getString("id"));
+	        serverLogin.setPasswd(b.getString("passwd"));
+        
+	        trace("WirelessInfo: onClickThrputTest: End. Got server to test = "+serverLogin.getHost());
+		} else {
+			Toast.makeText(this, "Please set Login Details first!", Toast.LENGTH_SHORT).show();
+		}
+		
+	}
+	
+	public void onClickConnect(View v) {
+		String buttonType = (String) connectFtpButton.getText();
+		ThrPutTest t = mCurrentThrPutTask;
+		if (t != null) {
+			connectFtpButton.setText("Disconnect");
+			trace("test: onClickConnect: Still connected, disconnect first");
+		} else {
+			ThrPutTest ft = new ThrPutTest(this, "filename", serverLogin);
+			mCurrentThrPutTask = ft;
+			connectFtpButton.setText("Disconnect");
+		}
+	}
+	
+	public void onClickftpAction(View v) {
+		ftpActionButton = (ToggleButton) findViewById(R.id.ftpAction);
+
+		ThrPutTest t = mCurrentThrPutTask;
+		if (t != null) {
+			trace("test: onClickConnect: Still connected, disconnect first");
+		} else {
+			ThrPutTest ft = new ThrPutTest(this, "filename", serverLogin);
+			mCurrentThrPutTask = ft;
+		}
+		
+		// Connect to FTP server 
+		if (ftpActionButton.isChecked()) {
+			trace("test: onClickftpAction: ToggelButton isChecked, will connect.");
+			mCurrentThrPutTask.mConnect();
+		} else if (mCurrentThrPutTask != null ){
+			trace("test: onClickftpAction: Will disconnect.");
+			mCurrentThrPutTask.mDisconnect();
+		}
 	}
 	
 	public void trace(String msg) {
