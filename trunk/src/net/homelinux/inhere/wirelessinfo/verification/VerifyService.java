c32efebff053d2cbe2669b1b98348027147ee950
pieter.linde <pieter.linde@1e14eff0-41d3-32bc-a52b-4516c52b40c5>
2011-08-03 05:45:47 +0000

    Created verification/VerifyService.java to handle all verifications.

--- trunk/src/net/homelinux/inhere/wirelessinfo/verification/VerifyService.java
+++ trunk/src/net/homelinux/inhere/wirelessinfo/verification/VerifyService.java
@@ -1,9 +1,35 @@
 package net.homelinux.inhere.wirelessinfo.verification;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.util.Log;
+import net.homelinux.inhere.wirelessinfo.test;
+import net.homelinux.inhere.wirelessinfo.database.WirelessInfoDBAdapter;
+
 public class VerifyService {
+	private Context context;
 	
-	private void serverInfo() {
-		
+	VerifyService(Context context) {
+		this.context = context;
+	}
+	
+	private boolean serverInfo() throws WirelessInfoException {
+		WirelessInfoDBAdapter dbAdapter = new WirelessInfoDBAdapter(context);
+		try {
+	    	dbAdapter.open();
+	    	trace("serverInfo: Opened DB");
+	    	dbAdapter.close();
+	    	return true;
+	    } catch (SQLException e) {
+	    	trace("serverInfo: Fail to open db "+e.getMessage());
+	    	dbAdapter.close();
+	    }
+
+		throw new WirelessInfoException("Test Server info do not exist");
+	}
+	
+	public void trace(String msg) {
+		Log.d("WirelessInfo", VerifyService.class.getName()+": "+msg);
 	}
 
 }
